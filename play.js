e15bc3bd819403e4e89c72064f94cbd7de79ce16
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-22 09:49:57 -0400

    add

--- play.js
+++ play.js
@@ -1,19 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-
-  
-  
-process.stdout.write('\x07');
-
-const { Game } = require('./src/Game')
-const { UserInterface } = require('./src/UserInterface')
-const { RemoteInterface } = require('./src/RemoteInterface')
-const game = new Game(new UserInterface(), new RemoteInterface())
-
-// Begin game
-game.start()
-
-
+const { connect } = require('./client');
+console.log('Connecting ...');
+connect();
