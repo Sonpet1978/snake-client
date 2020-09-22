c7ab75e59d23ed35ecc253d7f9c1b18983d81420
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-22 09:41:59 -0400

    add

---
+++ play.js
@@ -0,0 +1,35 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+//#!/usr/bin/env node
+const connect = function() {
+    const conn = net.createConnection({ 
+      host: '10.0.2.15',
+      port: 50541
+    });
+    // interpret incoming data as text
+    conn.setEncoding('utf8'); 
+  
+    return conn;
+  }
+  
+  
+  
+  console.log('Connecting ...');
+  connect(); 
+  
+  
+  
+process.stdout.write('\x07');
+
+const { Game } = require('./src/Game')
+const { UserInterface } = require('./src/UserInterface')
+const { RemoteInterface } = require('./src/RemoteInterface')
+const game = new Game(new UserInterface(), new RemoteInterface())
+
+// Begin game
+game.start()
+
+
